f953130d824f99a8fff4f372e043bb78f631143f
YukiKusumi <79245623+YukiKusumi@users.noreply.github.com>
2024-07-16 17:04:11 +0900

    11章終了

--- src/Franc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Vendor\PhpTdd;
-
-class Franc extends Money
-{
-}
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -22,25 +22,11 @@ class MoneyTest extends TestCase
         $fiveFran = Money::franc(5);
         $this->assertTrue($five->equals(Money::dollar(5)));
         $this->assertFalse($five->equals(Money::dollar(6)));
-        $this->assertTrue($fiveFran->equals(Money::franc(5)));
-        $this->assertFalse($fiveFran->equals(Money::franc(6)));
         $this->assertFalse($fiveFran->equals(Money::dollar(5)));
-    }
-
-    public function testFrancMultiplication()
-    {
-        $five = Money::franc(5);
-        $this->assertTrue(Money::franc(10)->equals($five->times(2)));
-        $this->assertTrue(Money::franc(15)->equals($five->times(3)));
     }
 
     public function testCurrency(){
         $this->assertEquals("USD", Money::dollar(1)->currency());
         $this->assertEquals("CHF", Money::franc(1)->currency());
-    }
-
-    public function testDifferentClassEquality()
-    {
-        $this->assertTrue((new Money(10, 'CHF'))->equals(new Franc(10, 'CHF')));
     }
 }
