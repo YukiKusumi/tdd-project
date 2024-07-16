8cbc16331e1678fe2e1089ca630d91c55c0e0192
YukiKusumi <79245623+YukiKusumi@users.noreply.github.com>
2024-07-16 16:07:18 +0900

    再度9章訂正

--- src/Franc.php
+++ src/Franc.php
@@ -4,12 +4,6 @@ namespace Vendor\PhpTdd;
 
 class Franc extends Money
 {
-    public function __construct(int $amount, string $currency)
-    {
-        $this->amount = $amount;
-        $this->currency = $currency;
-    }
-
     public function times(int $multiplier): Money
     {
         return Money::franc($this->amount * $multiplier);
