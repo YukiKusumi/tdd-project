aabc22e60e4bddd52c902e8bdba5b71d95b56610
YukiKusumi <79245623+YukiKusumi@users.noreply.github.com>
2024-08-06 15:48:37 +0900

    13章p94

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -53,4 +53,12 @@ class MoneyTest extends TestCase
         $result = $bank->reduce($sum, "USD");
         $this->assertEquals(Money::dollar(7), $result);
     }
+
+    public function testReduceMoney()
+    {
+        $bank = new Bank();
+        $result = $bank->reduce(Money::dollar(1), "USD");
+        
+        $this->assertEquals(Money::dollar(1), $result);
+    }
 }
