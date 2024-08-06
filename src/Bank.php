1e274c485dd3d2dfc35c4c707ef11b75b31e63df
YukiKusumi <79245623+YukiKusumi@users.noreply.github.com>
2024-08-06 15:45:20 +0900

    13章~p93-2

--- src/Bank.php
+++ src/Bank.php
@@ -7,7 +7,6 @@ class Bank
     public function reduce(Expression $source, string $to):Money
     {
         $sum = $source;
-//        $amount = $sum->augend->amount + $sum->addend->amount;
         return $sum->reduce($to);
     }
 }
